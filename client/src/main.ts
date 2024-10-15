43cbf4781174650d0e6615dd1f716a25c940cafd
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-10-15 19:05:04 +0300

    /

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,18 @@
+<<<<<<< HEAD
 import './style.scss';
+<<<<<<< Updated upstream
+
+=======
+=======
+>>>>>>> upstream/main
+>>>>>>> Stashed changes
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './style.scss'
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
 import { setupCounter } from './counter.ts'
 import { renderHeader } from './view/header/header.ts';
 import { renderDashboard } from './view/dashboard/dashboard.ts';
@@ -10,5 +21,67 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = renderHeader();
 document.querySelector<HTMLDivElement>('#dashboard')!.innerHTML = renderHeader();
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+=======
+>>>>>>> Stashed changes
+import { courses } from './models/entities/Course.ts';
+import { getCourseSubjects } from './controllers/courseSubjectsCont.ts';
+import { testCourseSubjects } from './models/joinTables/coursesSubjectModel.ts';
+import { renderCourseSubjects } from './view/courseSubjects/courseSubjectView.ts';
+import { renderSelectCourse } from './view/components/selectCourse/renderSelectCourse.ts';
+
+
+
+try {
+    testCourseSubjects();
+
+
+    renderFirstPage();
+}
+catch (error) {
+    console.error(error);
+}
+
+
+function renderFirstPage(courseId?: string) {
+    try {
+        const mainPage = document.querySelector<HTMLDivElement>('#app')
+        if (!mainPage) throw new Error("main page not found");
+
+        const course = courses.find(u => u.id === courseId);
+
+        if (!course) {
+            mainPage.innerHTML = `
+             ${renderSelectCourse(courses)}
+       `;
+        } else {
+            const courseSubjects = getCourseSubjects(course);
+
+            mainPage.innerHTML = `
+           ${renderSelectCourse(courses)}
+           ${renderCourseSubjects(course, courseSubjects)}
+           `;
+        }
+
+
+
+        const courseSelection = document.querySelector<HTMLSelectElement>('#course-selection');
+        if (!courseSelection) throw new Error("course selection not found");
+
+        courseSelection.addEventListener('change', (event: any) => {
+            console.log("change")
+            const courseId = event.target.value;
+            console.log(courseId)
+            renderFirstPage(courseId);
+        })
+    }
+    catch (error) {
+        console.error(error);
+        return ""
+    }
+}
+<<<<<<< Updated upstream
+=======
+>>>>>>> upstream/main
+>>>>>>> Stashed changes
 
 
